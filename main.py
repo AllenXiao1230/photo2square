0c899c87716065df7a62debf124ac2358dc63c40
AllenXiao <dabao20041230@gmail.com>
2023-10-01 09:20:03 +0800

    1001

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageDraw
 import os
 
+output_size = (1920, 1920) #(X, Y)
+
 input_link = input("請輸入資料夾位址:")
 link_list = os.listdir(input_link)
 
@@ -12,7 +14,7 @@ for file_name in link_list:
     text = '轉換進度: ' + str(count) + ' /' + str(total_file_count)
     print(text, end="\r")
 
-    bg = Image.new("RGB", (1920, 1920), color = (255, 255, 255))
+    bg = Image.new("RGB", output_size, color = (255, 255, 255))
     drew = ImageDraw.Draw(bg)
 
     full_link = input_link + "\\" + file_name
@@ -21,24 +23,24 @@ for file_name in link_list:
     if ft.size[0] > ft.size[1]:
         #橫
         
-        ft = ft.resize((1920, int(ft.size[1] * 1920 / ft.size[0])))
-        bg.paste(ft, (0, 960 - int(ft.size[1] / 2)))
+        ft = ft.resize((output_size[0], int(ft.size[1] * output_size[0] / ft.size[0])))
+        bg.paste(ft, (0, int(output_size[1] / 2) - int(ft.size[1] / 2)))
 
         bg.save('output\\' + file_name + ".jpg")
 
         count += 1
-        # print(str(count) + '/' + str(total_file_count), flush=True)
+        
 
     else:
         #直
 
-        ft = ft.resize((int(ft.size[0] * 1920 / ft.size[1]), 1920))
-        bg.paste(ft, (960 - int(ft.size[0] / 2), 0))
+        ft = ft.resize((int(ft.size[0] * output_size[1] / ft.size[1]), output_size[1]))
+        bg.paste(ft, (int(output_size[0] / 2) - int(ft.size[0] / 2), 0))
 
         bg.save('output\\' + file_name + ".jpg")
 
         count += 1
-        # print(str(count) + '/' + str(total_file_count), flush=True)
+        
 
 
 print('完成，共計 ' + str(count) + '個檔案')
