cecaeb528e4571949d2688c4735733e4442cbe9f
Allen is coding <85085824+AllenXiao1230@users.noreply.github.com>
2024-02-01 08:43:38 +0800

    1.0

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+
 from PIL import Image, ImageDraw
 import os
 
 output_size = (1920, 1920) #(X, Y)
+
+color = input("選擇背景顏色:\n1. 黑色\n2. 白色\n")
+if color == "2":
+    bg_color = (255, 255, 255)
+else:
+    bg_color = (0, 0, 0)
 
 input_link = input("請輸入資料夾位址:")
 link_list = os.listdir(input_link)
@@ -14,7 +21,7 @@ for file_name in link_list:
     text = '轉換進度: ' + str(count) + ' /' + str(total_file_count)
     print(text, end="\r")
 
-    bg = Image.new("RGB", output_size, color = (255, 255, 255))
+    bg = Image.new("RGB", output_size, color = bg_color)
     drew = ImageDraw.Draw(bg)
 
     full_link = input_link + "\\" + file_name
@@ -26,7 +33,7 @@ for file_name in link_list:
         ft = ft.resize((output_size[0], int(ft.size[1] * output_size[0] / ft.size[0])))
         bg.paste(ft, (0, int(output_size[1] / 2) - int(ft.size[1] / 2)))
 
-        bg.save('output\\' + file_name + ".jpg")
+        bg.save('C:\\Allen\\photo2square\\output\\' + file_name )
 
         count += 1
         
@@ -37,7 +44,7 @@ for file_name in link_list:
         ft = ft.resize((int(ft.size[0] * output_size[1] / ft.size[1]), output_size[1]))
         bg.paste(ft, (int(output_size[0] / 2) - int(ft.size[0] / 2), 0))
 
-        bg.save('output\\' + file_name + ".jpg")
+        bg.save('C:\\Allen\\photo2square\\output\\' + file_name )
 
         count += 1
         
